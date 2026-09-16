37dbe8c8ecc958f5f0a4a7caaa4a75d126377874
Atul Gaikwad <atulgaikwad2303@gmail.com>
2026-09-16 16:18:49 +0530

    Time: 342 ms (24.17%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,1 +1,1 @@
-SELECT (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee);
